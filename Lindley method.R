c0d3f336e9a6bcf06163bf10a453209e54b148ed
nsrnmj <66920580+nsrnmj@users.noreply.github.com>
2024-12-29 16:51:12 +0330

    Update Lindley method.R
    کد مربوط به برآورد پارامترها به روش لیندلی

--- Lindley method.R
+++ Lindley method.R
@@ -1,18 +1,3 @@
-R=c(n-m,c(rep(0,m-1)));theta=c(a,b,c);V=U=c()‎
-
-‎for(j in 1:m){‎
-‎for(i in 1:m){‎
-‎W=sort(runif(m,min=0,max=1))‎
-‎V[i]=W[i]^(1/(i+(sum(R[(m-i+1):m]))))}‎
-‎U[j]=1-prod(V[(m-j+1):m])}‎
-‎y=((-1/b)*log(1-(U^(1/a))))^(1/c)‎
-‎print(y)‎
-‎\end{lstlisting}‎
-‎\end{latin}‎
-
-\section*{کد مربوط به برآورد پارامترها به روش لیندلی}
-‎\begin{latin}‎
-‎\begin{lstlisting}[language=R]‎
 ‎L1=function(theta){‎
 ‎a=theta[1];b=theta[2];c=theta[3]‎
 ‎for(j in 1:m){‎
